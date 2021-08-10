cd8888128c8c70caf344e3b184139e5d8c314c31
Jay Gudsson <gudsson@gmail.com>
2021-08-10 00:11:09 -0700

    step 5: add pagination to table

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,23 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 const Table = ({ className, columns, rows, format }) => {
+  const [startingRow, setStartingRow] = useState(0)
+  // const [finishingRow, setfinishingRow] = useState(PAGE_SIZE)
+  const perPage = 25
+  const totalRows = rows.length
+  const displayRows = rows.slice(startingRow, startingRow + perPage)
+
+  
+  const prevPage = (event) => {
+    setStartingRow(startingRow - perPage)
+  }
+  const nextPage = (event) => {
+    setStartingRow(startingRow + perPage)
+  }
+
   return (
-    <table className={className}>
-      <thead>
-        <tr>
-          {columns.map(column => 
-            <th>{column.name}</th>
+    <div>
+      <table className={className}>
+        <thead>
+          <tr>
+            {columns.map(column => 
+              <th>{column.name}</th>
+            )}
+          </tr>
+        </thead>
+        <tbody>
+          {displayRows.map((row, idx) => 
+            <tr key={idx}>{columns.map(column => 
+              <td>{format(column.property, row[column.property])}</td>
+            )}</tr>
           )}
-        </tr>
-      </thead>
-      <tbody>
-        {rows.map((row, idx) => 
-          <tr key={idx}>{columns.map(column => 
-            <td>{format(column.property, row[column.property])}</td>
-          )}</tr>
-        )}
-      </tbody>
-    </table>
+        </tbody>
+      </table>
+      <p>Showing {startingRow + 1}-
+        {startingRow + perPage >= totalRows
+          ? totalRows
+          : startingRow + perPage} of {totalRows} routes</p>
+      <p>
+        <button onClick={prevPage} disabled={startingRow === 0}>Previous Page</button>
+        <button onClick={nextPage} disabled={startingRow + perPage >= totalRows}>Next Page</button>
+      </p>
+    </div>
   )
 }
 
